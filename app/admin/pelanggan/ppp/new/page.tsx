e5b959d2a21cfa9ab18cf7ab477276f5a1c4622a
Rohadim Raja <rohadimraja@example.com>
2026-02-21 20:52:13 +0700

    Menghapus halaman pembuatan pelanggan PPP baru dan komponen kliennya.

--- app/admin/pelanggan/ppp/new/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ensurePermission } from '@/lib/rbac'
-import { ClientComponent } from './PppNewClient'
-
-export default async function Page() {
-    await ensurePermission('pelanggan:create')
-    return <ClientComponent />
-}
