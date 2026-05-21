8a69feafe66026566acf1032654dc7a084112f19
Rohadi M Raja <rohadimraja@icloud.com>
2026-05-21 16:02:43 +0700

    feat(salary-v2): add core and module-level barrel exports

---
+++ modules/salary-v2/core/index.ts
@@ -0,0 +1,94 @@
+// Domain - Entities
+export type {
+  PayrollRun,
+  PayrollEntry,
+  PayrollLine,
+  PayrollComponent,
+  EmployeePayrollProfile,
+  BpjsEnrollment,
+  EmployeeComponent,
+  PaySchedule,
+  PayrollPeriod,
+  PayrollAuditLog,
+  AuditChange,
+  AuditEntityType,
+  SalaryAdvance,
+  DeductionMethod,
+  RegionalMinimumWage,
+} from "./domain/entities";
+
+// Domain - Enums
+export {
+  PayrollRunStatus,
+  PayrollEntryStatus,
+  PayrollRunType,
+  PayrollPeriodStatus,
+  EmployeeType,
+  TaxMethod,
+  PayFrequency,
+  ComponentCategory,
+  ComponentCalculationType,
+  PaymentBatchStatus,
+  PaymentItemStatus,
+  SalaryAdvanceStatus,
+  AuditAction,
+  ComplianceSeverity,
+  OvertimeDayType,
+  OvertimeCapEnforcement,
+} from "./domain/enums";
+
+// Domain - Value Objects
+export { Money } from "./domain/value-objects";
+export { Period } from "./domain/value-objects";
+export { PtkpStatus, PTKP_CATEGORIES } from "./domain/value-objects";
+export type { PtkpCategory } from "./domain/value-objects";
+
+// Domain - Ports
+export type {
+  IPayrollRunRepository,
+  PayrollRunFilter,
+  IPayrollEntryRepository,
+  PayrollEntryWithLines,
+  IPayrollComponentRepository,
+  ComponentFilter,
+  IEmployeePayrollProfileRepository,
+  ProfileFilter,
+  IPayScheduleRepository,
+  IPayrollPeriodRepository,
+  PeriodFilter,
+  ISalaryAdvanceRepository,
+  AdvanceFilter,
+  IRegionalMinimumWageRepository,
+  IPayrollCalculator,
+  CalculationContext,
+  CalculationResult,
+  AttendanceSummary,
+  OvertimeSummary,
+} from "./domain/ports";
+
+// Config
+export type {
+  TenantPayrollConfig,
+  ThrConfig,
+  SalaryAdvancePolicy,
+  PeriodLockingPolicy,
+  BpjsRateConfig,
+  BpjsProgramRate,
+  BpjsEmployerOnlyRate,
+  TenantTaxConfig,
+  ProgressiveRate,
+  TerBracket,
+  PtkpAmount,
+  OvertimeConfig,
+  OvertimeTier,
+} from "./config";
+
+export {
+  DEFAULT_BPJS_CONFIG,
+  DEFAULT_TAX_CONFIG,
+  DEFAULT_OVERTIME_CONFIG,
+} from "./config";
+
+// Errors
+export { PayrollError } from "./errors";
+export type { PayrollErrorCode } from "./errors";
